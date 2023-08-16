34ceb37e01ba83f71d8a6039f9f6ea24a7567998
Ukasha Zia <ukashazia41@gmail.com>
2023-08-16 16:46:13 +0500

    cleaned and formatted

--- test/support/fixtures/users_fixtures.ex
+++ test/support/fixtures/users_fixtures.ex
@@ -4,28 +4,28 @@ defmodule Doppler.UsersFixtures do
   entities via the `Doppler.Users` context.
   """
 
-  def unique_user_email, do: "user#{System.unique_integer()}@example.com"
-  def valid_user_password, do: "hello world!"
+  # def unique_user_email, do: "user#{System.unique_integer()}@example.com"
+  # def valid_user_password, do: "hello world!"
 
-  def valid_user_attributes(attrs \\ %{}) do
-    Enum.into(attrs, %{
-      email: unique_user_email(),
-      password: valid_user_password()
-    })
-  end
+  # def valid_user_attributes(attrs \\ %{}) do
+  #   Enum.into(attrs, %{
+  #     email: unique_user_email(),
+  #     password: valid_user_password()
+  #   })
+  # end
 
-  def user_fixture(attrs \\ %{}) do
-    {:ok, user} =
-      attrs
-      |> valid_user_attributes()
-      |> Doppler.Users.register_user()
+  # def user_fixture(attrs \\ %{}) do
+  #   {:ok, user} =
+  #     attrs
+  #     |> valid_user_attributes()
+  #     |> Doppler.Users.register_user()
 
-    user
-  end
+  #   user
+  # end
 
-  def extract_user_token(fun) do
-    {:ok, captured_email} = fun.(&"[TOKEN]#{&1}[TOKEN]")
-    [_, token | _] = String.split(captured_email.text_body, "[TOKEN]")
-    token
-  end
+  # def extract_user_token(fun) do
+  #   {:ok, captured_email} = fun.(&"[TOKEN]#{&1}[TOKEN]")
+  #   [_, token | _] = String.split(captured_email.text_body, "[TOKEN]")
+  #   token
+  # end
 end
